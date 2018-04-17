2dd8789f5c8c58d6031b0120884ae815d5b23490
Ahmadali <ahmadali.azizov@outlook.com>
2018-04-17 20:49:11 +0000

    automatically backed up by learn

---
+++ loops.js
@@ -0,0 +1,12 @@
+function forLoop(array){
+  
+    for(var i = 0 ; i < 25; i++) {
+    
+    var s = i === 1 ? "I am 1 strange loop." : `I am ${i} strange loops`;
+      
+      array.push(s);
+      
+      return array;
+      
+    }
+    }
