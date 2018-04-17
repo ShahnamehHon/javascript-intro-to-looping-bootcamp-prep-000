4ce2e1d2c01f8a0736a3d175afb726fd90d9b52b
Ahmadali <ahmadali.azizov@outlook.com>
2018-04-17 21:03:07 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -2,7 +2,8 @@ function forLoop(array) {
   
     for(let i = 1 ; i < 25; i++) {
     
-    let s = i === 0 ? "I am 1 strange loop." : `I am ${i} strange loops`;
+    let s = i === 0 ? "I am 1 strange loop."
+                        : `I am ${i} strange loops`;
       
       array.push(s);
       
